5f1fedec80bc9d285711402d49437164c00fe497
michal dub <michal.dub@sanomamedia.cz>
2015-01-07 15:07:01 +0100

    added init migrations for django 1.7 and moved south migrations to south_migrations directories

--- ella/core/migrations/0001_initial.py
+++ ella/core/migrations/0001_initial.py
@@ -1,154 +1,158 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
-
-class Migration(SchemaMigration):
-
-    def forwards(self, orm):
-        # Adding model 'Author'
-        db.create_table('core_author', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True, blank=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=200, blank=True)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=255, db_index=True)),
-            ('description', self.gf('django.db.models.fields.TextField')(blank=True)),
-            ('text', self.gf('django.db.models.fields.TextField')(blank=True)),
-            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75, blank=True)),
-        ))
-        db.send_create_signal('core', ['Author'])
-
-        # Adding model 'Source'
-        db.create_table('core_source', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('url', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
-            ('description', self.gf('django.db.models.fields.TextField')(blank=True)),
-        ))
-        db.send_create_signal('core', ['Source'])
-
-        # Adding model 'Category'
-        db.create_table('core_category', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=255, db_index=True)),
-            ('tree_parent', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Category'], null=True, blank=True)),
-            ('tree_path', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('description', self.gf('django.db.models.fields.TextField')(blank=True)),
-            ('site', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['sites.Site'])),
-        ))
-        db.send_create_signal('core', ['Category'])
-
-        # Adding unique constraint on 'Category', fields ['site', 'tree_path']
-        db.create_unique('core_category', ['site_id', 'tree_path'])
-
-        # Adding model 'Dependency'
-        db.create_table('core_dependency', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('target_ct', self.gf('django.db.models.fields.related.ForeignKey')(related_name='dependency_for_set', to=orm['contenttypes.ContentType'])),
-            ('target_id', self.gf('django.db.models.fields.IntegerField')()),
-            ('dependent_ct', self.gf('django.db.models.fields.related.ForeignKey')(related_name='depends_on_set', to=orm['contenttypes.ContentType'])),
-            ('dependent_id', self.gf('django.db.models.fields.IntegerField')()),
-        ))
-        db.send_create_signal('core', ['Dependency'])
+import re
+import django.db.models.deletion
+import app_data.fields
+from django.conf import settings
+import ella.core.cache.fields
+import django.core.validators
 
 
-    def backwards(self, orm):
-        # Removing unique constraint on 'Category', fields ['site', 'tree_path']
-        db.delete_unique('core_category', ['site_id', 'tree_path'])
+class Migration(migrations.Migration):
 
-        # Deleting model 'Author'
-        db.delete_table('core_author')
+    dependencies = [
+        ('sites', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('contenttypes', '0001_initial'),
+    ]
 
-        # Deleting model 'Source'
-        db.delete_table('core_source')
-
-        # Deleting model 'Category'
-        db.delete_table('core_category')
-
-        # Deleting model 'Dependency'
-        db.delete_table('core_dependency')
-
-
-    models = {
-        'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        'auth.permission': {
-            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        'auth.user': {
-            'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
-        },
-        'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        'core.author': {
-            'Meta': {'object_name': 'Author'},
-            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '255', 'db_index': 'True'}),
-            'text': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'})
-        },
-        'core.category': {
-            'Meta': {'ordering': "('site__name', 'tree_path')", 'unique_together': "(('site', 'tree_path'),)", 'object_name': 'Category'},
-            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['sites.Site']"}),
-            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '255', 'db_index': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
-            'tree_parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Category']", 'null': 'True', 'blank': 'True'}),
-            'tree_path': ('django.db.models.fields.CharField', [], {'max_length': '255'})
-        },
-        'core.dependency': {
-            'Meta': {'ordering': "('dependent_ct', 'dependent_id')", 'object_name': 'Dependency'},
-            'dependent_ct': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'depends_on_set'", 'to': "orm['contenttypes.ContentType']"}),
-            'dependent_id': ('django.db.models.fields.IntegerField', [], {}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'target_ct': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'dependency_for_set'", 'to': "orm['contenttypes.ContentType']"}),
-            'target_id': ('django.db.models.fields.IntegerField', [], {})
-        },
-        'core.source': {
-            'Meta': {'object_name': 'Source'},
-            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
-            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'})
-        },
-        'sites.site': {
-            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
-            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        }
-    }
-
-    complete_apps = ['core']
+    operations = [
+        migrations.CreateModel(
+            name='Author',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200, verbose_name='Name', blank=True)),
+                ('slug', models.SlugField(unique=True, max_length=255, verbose_name='Slug', validators=[django.core.validators.RegexValidator(re.compile('^[-a-zA-Z0-9_]+$'), "Enter a valid 'slug' consisting of letters, numbers, underscores or hyphens.", 'invalid')])),
+                ('description', models.TextField(verbose_name='Description', blank=True)),
+                ('text', models.TextField(verbose_name='Text', blank=True)),
+                ('email', models.EmailField(max_length=75, verbose_name='Email', blank=True)),
+                ('user', ella.core.cache.fields.CachedForeignKey(verbose_name='User', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+            options={
+                'verbose_name': 'Author',
+                'verbose_name_plural': 'Authors',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=200, verbose_name='Title')),
+                ('description', models.TextField(help_text='Description which can be used in link titles, syndication etc.', verbose_name='Description', blank=True)),
+                ('content', models.TextField(default=b'', help_text='Optional content to use when rendering this category.', verbose_name='Content', blank=True)),
+                ('template', models.CharField(default=b'category.html', help_text='Template to use to render detail page of this category.', max_length=100, verbose_name='Template', choices=[(b'category.html', 'default (category.html)')])),
+                ('slug', models.SlugField(max_length=255, verbose_name='Slug', validators=[django.core.validators.RegexValidator(re.compile(b'^(?:[0-9]+[^0-9-]|[a-z])[a-z0-9-]*$'), 'Please enter a valid slug composed of lowecase letter, numbers and hyphens. First character must be a letter.', b'invalid')])),
+                ('tree_path', models.CharField(verbose_name='Path from root category', max_length=255, editable=False)),
+                ('app_data', app_data.fields.AppDataField(default=b'{}', help_text='If you need to define custom data for category objects, you can use this field to do so.', verbose_name='Custom meta data', editable=False)),
+                ('site', ella.core.cache.fields.SiteForeignKey(to='sites.Site')),
+                ('tree_parent', ella.core.cache.fields.CategoryForeignKey(verbose_name='Parent category', blank=True, to='core.Category', null=True)),
+            ],
+            options={
+                'verbose_name': 'Category',
+                'verbose_name_plural': 'Categories',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Dependency',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('target_id', models.IntegerField()),
+                ('dependent_id', models.IntegerField()),
+                ('dependent_ct', ella.core.cache.fields.ContentTypeForeignKey(related_name='depends_on_set', to='contenttypes.ContentType')),
+                ('target_ct', ella.core.cache.fields.ContentTypeForeignKey(related_name='dependency_for_set', to='contenttypes.ContentType')),
+            ],
+            options={
+                'verbose_name': 'Dependency',
+                'verbose_name_plural': 'Dependencies',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Listing',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('publish_from', models.DateTimeField(verbose_name='Start of listing', db_index=True)),
+                ('publish_to', models.DateTimeField(null=True, verbose_name='End of listing', blank=True)),
+                ('commercial', models.BooleanField(default=False, help_text='Check this if the listing is of a commercial content.', verbose_name='Commercial')),
+                ('category', ella.core.cache.fields.CategoryForeignKey(verbose_name='Category', to='core.Category')),
+            ],
+            options={
+                'verbose_name': 'Listing',
+                'verbose_name_plural': 'Listings',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Publishable',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=255, verbose_name='Title')),
+                ('slug', models.SlugField(max_length=255, verbose_name='Slug', validators=[django.core.validators.RegexValidator(re.compile('^[-a-zA-Z0-9_]+$'), "Enter a valid 'slug' consisting of letters, numbers, underscores or hyphens.", 'invalid')])),
+                ('description', models.TextField(verbose_name='Description', blank=True)),
+                ('published', models.BooleanField(default=False, verbose_name='Published')),
+                ('publish_from', models.DateTimeField(default=datetime.datetime(3000, 1, 1, 0, 0), verbose_name='Publish from', db_index=True)),
+                ('publish_to', models.DateTimeField(null=True, verbose_name='End of visibility', blank=True)),
+                ('static', models.BooleanField(default=False, verbose_name='static')),
+                ('last_updated', models.DateTimeField(verbose_name='Last updated', blank=True)),
+                ('app_data', app_data.fields.AppDataField(default=b'{}', editable=False)),
+                ('announced', models.BooleanField(default=False, help_text=b'Publish signal sent', editable=False)),
+                ('authors', models.ManyToManyField(to='core.Author', verbose_name='Authors')),
+                ('category', ella.core.cache.fields.CategoryForeignKey(verbose_name='Category', to='core.Category')),
+                ('content_type', ella.core.cache.fields.ContentTypeForeignKey(editable=False, to='contenttypes.ContentType')),
+            ],
+            options={
+                'verbose_name': 'Publishable object',
+                'verbose_name_plural': 'Publishable objects',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Related',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('related_id', models.IntegerField(verbose_name='Object ID')),
+                ('publishable', models.ForeignKey(verbose_name='Publishable', to='core.Publishable')),
+                ('related_ct', ella.core.cache.fields.ContentTypeForeignKey(verbose_name='Content type', to='contenttypes.ContentType')),
+            ],
+            options={
+                'verbose_name': 'Related',
+                'verbose_name_plural': 'Related',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Source',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200, verbose_name='Name')),
+                ('url', models.URLField(verbose_name='URL', blank=True)),
+                ('description', models.TextField(verbose_name='Description', blank=True)),
+            ],
+            options={
+                'verbose_name': 'Source',
+                'verbose_name_plural': 'Sources',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='publishable',
+            name='source',
+            field=ella.core.cache.fields.CachedForeignKey(on_delete=django.db.models.deletion.SET_NULL, verbose_name='Source', blank=True, to='core.Source', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='listing',
+            name='publishable',
+            field=ella.core.cache.fields.CachedForeignKey(verbose_name='Publishable', to='core.Publishable'),
+            preserve_default=True,
+        ),
+        migrations.AlterUniqueTogether(
+            name='category',
+            unique_together=set([('site', 'tree_path')]),
+        ),
+    ]
--- ella/positions/migrations/0001_initial.py
+++ ella/positions/migrations/0001_initial.py
@@ -1,63 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-from south.db import db
-from django.db import models
-from ella.positions.models import *
+from django.db import models, migrations
+import ella.core.cache.fields
 
-class Migration:
-    
-    depends_on = (
-        ("core", "0001_initial"),
-    )
- 
-    def forwards(self, orm):
-        
-        # Adding model 'Position'
-        db.create_table('positions_position', (
-            ('id', models.AutoField(primary_key=True)),
-            ('category', models.ForeignKey(orm['core.Category'], verbose_name=_('Category'))),
-            ('name', models.CharField(_('Name'), max_length=200)),
-            ('target_ct', models.ForeignKey(orm['contenttypes.ContentType'], null=True, verbose_name=_('Target content type'), blank=True)),
-            ('target_id', models.PositiveIntegerField(_('Target id'), null=True, blank=True)),
-            ('active_from', models.DateTimeField(_('Position active from'), null=True, blank=True)),
-            ('active_till', models.DateTimeField(_('Position active till'), null=True, blank=True)),
-            ('box_type', models.CharField(_('Box type'), max_length=200, blank=True)),
-            ('text', models.TextField(_('Definition'), blank=True)),
-            ('disabled', models.BooleanField(_('Disabled'), default=False)),
-        ))
-        db.send_create_signal('positions', ['Position'])
-        
-    
-    
-    def backwards(self, orm):
-        
-        # Deleting model 'Position'
-        db.delete_table('positions_position')
-        
-    
-    
-    models = {
-        'core.category': {
-            'Meta': {'ordering': "('site','tree_path',)", 'unique_together': "(('site','tree_path'),)"},
-            '_stub': True,
-            'id': ('models.AutoField', [], {'primary_key': 'True'})
-        },
-        'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label','model'),)", 'db_table': "'django_content_type'"},
-            '_stub': True,
-            'id': ('models.AutoField', [], {'primary_key': 'True'})
-        },
-        'positions.position': {
-            'active_from': ('models.DateTimeField', ["_('Position active from')"], {'null': 'True', 'blank': 'True'}),
-            'active_till': ('models.DateTimeField', ["_('Position active till')"], {'null': 'True', 'blank': 'True'}),
-            'box_type': ('models.CharField', ["_('Box type')"], {'max_length': '200', 'blank': 'True'}),
-            'category': ('models.ForeignKey', ["orm['core.Category']"], {'verbose_name': "_('Category')"}),
-            'disabled': ('models.BooleanField', ["_('Disabled')"], {'default': 'False'}),
-            'id': ('models.AutoField', [], {'primary_key': 'True'}),
-            'name': ('models.CharField', ["_('Name')"], {'max_length': '200'}),
-            'target_ct': ('models.ForeignKey', ["orm['contenttypes.ContentType']"], {'null': 'True', 'verbose_name': "_('Target content type')", 'blank': 'True'}),
-            'target_id': ('models.PositiveIntegerField', ["_('Target id')"], {'null': 'True', 'blank': 'True'}),
-            'text': ('models.TextField', ["_('Definition')"], {'blank': 'True'})
-        }
-    }
-    
-    complete_apps = ['positions']
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contenttypes', '0001_initial'),
+        ('core', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Position',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200, verbose_name='Name')),
+                ('target_id', models.PositiveIntegerField(null=True, verbose_name='Target id', blank=True)),
+                ('text', models.TextField(verbose_name='Definition', blank=True)),
+                ('box_type', models.CharField(max_length=200, verbose_name='Box type', blank=True)),
+                ('active_from', models.DateTimeField(null=True, verbose_name='Position active from', blank=True)),
+                ('active_till', models.DateTimeField(null=True, verbose_name='Position active till', blank=True)),
+                ('disabled', models.BooleanField(default=False, verbose_name='Disabled')),
+                ('category', ella.core.cache.fields.CategoryForeignKey(verbose_name='Category', to='core.Category')),
+                ('target_ct', ella.core.cache.fields.ContentTypeForeignKey(verbose_name='Target content type', blank=True, to='contenttypes.ContentType', null=True)),
+            ],
+            options={
+                'verbose_name': 'Position',
+                'verbose_name_plural': 'Positions',
+            },
+            bases=(models.Model,),
+        ),
+    ]
