ec5859f5cefd543bc4a16416b0c63ed306e156b7
Petr Losa <petr@olc.cz>
2015-05-26 07:59:40 +0200

    migrace YT video

---
+++ ella/core/migrations/0012_auto__chg_field_listing_priority__add_field_publishable_youtube_video_.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Listing.priority'
+        db.alter_column(u'core_listing', 'priority', self.gf('django.db.models.fields.PositiveSmallIntegerField')(null=True))
+        # Adding field 'Publishable.youtube_video'
+        db.add_column(u'core_publishable', 'youtube_video',
+                      self.gf('django.db.models.fields.CharField')(max_length=2048, null=True, blank=True),
+                      keep_default=False)
+
+
+        # Changing field 'Publishable.priority'
+        db.alter_column(u'core_publishable', 'priority', self.gf('django.db.models.fields.PositiveSmallIntegerField')(null=True))
+
+        # Changing field 'Author.user'
+        db.alter_column(u'core_author', 'user_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['mpUser.User'], null=True))
+
+    def backwards(self, orm):
+
+        # Changing field 'Listing.priority'
+        db.alter_column(u'core_listing', 'priority', self.gf('django.db.models.fields.PositiveSmallIntegerField')())
+        # Deleting field 'Publishable.youtube_video'
+        db.delete_column(u'core_publishable', 'youtube_video')
+
+
+        # Changing field 'Publishable.priority'
+        db.alter_column(u'core_publishable', 'priority', self.gf('django.db.models.fields.PositiveSmallIntegerField')())
+
+        # Changing field 'Author.user'
+        db.alter_column(u'core_author', 'user_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True))
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'core.author': {
+            'Meta': {'object_name': 'Author'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'photo': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['photos.Photo']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '255'}),
+            'text': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['mpUser.User']", 'null': 'True', 'blank': 'True'})
+        },
+        'core.category': {
+            'Meta': {'unique_together': "(('site', 'tree_path'),)", 'object_name': 'Category'},
+            'app_data': ('app_data.fields.AppDataField', [], {'default': "'{}'"}),
+            'content': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['sites.Site']"}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '255'}),
+            'template': ('django.db.models.fields.CharField', [], {'default': "'category.html'", 'max_length': '100'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'tree_parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Category']", 'null': 'True', 'blank': 'True'}),
+            'tree_path': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'core.dependency': {
+            'Meta': {'object_name': 'Dependency'},
+            'dependent_ct': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'depends_on_set'", 'to': u"orm['contenttypes.ContentType']"}),
+            'dependent_id': ('django.db.models.fields.IntegerField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'target_ct': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'dependency_for_set'", 'to': u"orm['contenttypes.ContentType']"}),
+            'target_id': ('django.db.models.fields.IntegerField', [], {})
+        },
+        'core.listing': {
+            'Meta': {'object_name': 'Listing'},
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Category']"}),
+            'commercial': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'priority': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '1', 'null': 'True', 'blank': 'True'}),
+            'publish_from': ('django.db.models.fields.DateTimeField', [], {'db_index': 'True'}),
+            'publish_to': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'publishable': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Publishable']"})
+        },
+        'core.publishable': {
+            'Meta': {'object_name': 'Publishable'},
+            'announced': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'app_data': ('app_data.fields.AppDataField', [], {'default': "'{}'"}),
+            'authors': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['core.Author']", 'symmetrical': 'False'}),
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Category']"}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'description_cs': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'description_en': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_updated': ('django.db.models.fields.DateTimeField', [], {'blank': 'True'}),
+            'photo': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['photos.Photo']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'priority': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '1', 'null': 'True', 'blank': 'True'}),
+            'publish_from': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(3000, 1, 1, 0, 0)', 'db_index': 'True'}),
+            'publish_to': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'published': ('django.db.models.fields.BooleanField', [], {}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '255'}),
+            'slug_cs': ('django.db.models.fields.SlugField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'slug_en': ('django.db.models.fields.SlugField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'source': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Source']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'static': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'title_cs': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'title_en': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'youtube_video': ('django.db.models.fields.CharField', [], {'max_length': '2048', 'null': 'True', 'blank': 'True'})
+        },
+        'core.related': {
+            'Meta': {'object_name': 'Related'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'publishable': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Publishable']"}),
+            'related_ct': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            'related_id': ('django.db.models.fields.IntegerField', [], {})
+        },
+        'core.source': {
+            'Meta': {'object_name': 'Source'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'})
+        },
+        u'mpUser.user': {
+            'Meta': {'object_name': 'User'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'}),
+            'company': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'}),
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'postcode': ('django.db.models.fields.CharField', [], {'max_length': '5', 'blank': 'True'}),
+            'tax': ('django.db.models.fields.CharField', [], {'max_length': '15', 'blank': 'True'}),
+            'tin': ('django.db.models.fields.CharField', [], {'max_length': '10', 'blank': 'True'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'photos.photo': {
+            'Meta': {'object_name': 'Photo'},
+            'app_data': ('app_data.fields.AppDataField', [], {'default': "'{}'"}),
+            'authors': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'photo_set'", 'symmetrical': 'False', 'to': "orm['core.Author']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'height': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '255'}),
+            'important_bottom': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'important_left': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'important_right': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'important_top': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '255'}),
+            'source': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Source']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'width': ('django.db.models.fields.PositiveIntegerField', [], {})
+        },
+        u'sites.site': {
+            'Meta': {'ordering': "(u'domain',)", 'object_name': 'Site', 'db_table': "u'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['core']
